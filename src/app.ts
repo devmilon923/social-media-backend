cdc7aac4c9dd8f579d3f9219a57e6b81648a69be
devmilon923 <dev.milon923@gmail.com>
2025-11-25 22:06:39 +0600

    update cors

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "https://social-media-nine-phi.vercel.app",
+      "http://localhost:3000",
+    ],
     credentials: true,
   })
 );
